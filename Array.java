bd46091b290514099e71246e05d1db5b3f6ef74b
songE <seungmin576@gmail.com>
2024-12-24 12:33:21 +0900

    reverse array

---
+++ Array.java
@@ -0,0 +1,12 @@
+package programmers;
+
+public class Array {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[num_list.length];
+        int j = 0;
+        for (int i = num_list.length - 1; i >= 0; i--, j++) {
+            answer[j] = num_list[i];
+        }
+        return answer;
+    }
+}
---
+++ Test.java
@@ -0,0 +1,14 @@
+package programmers;
+
+import java.util.Arrays;
+
+public class Test {
+    public static void main(String[] args) {
+//        NumDivision numDivision = new NumDivision();
+//        System.out.println(numDivision.solution(10, 3));
+
+        Array array = new Array();
+        int[] list = {1, 2, 3, 4, 5};
+        System.out.println(Arrays.toString(array.solution(list)));
+    }
+}
